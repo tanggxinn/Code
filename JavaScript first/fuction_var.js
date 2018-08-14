92d39f819cb034b2620ad81cf9d73e3320624368
Yating <lijushang@gmail.com>
2018-08-14 16:14:15 +0800

    new homework

--- JavaScript first/fuction_var.js
+++ JavaScript first/fuction_var.js
@@ -1,9 +1,13 @@
 'use strict';
 
+//定义一个变量组名，后面用变量组名.变量就都不用申明了
+
 var myvar = {};
 myvar.x = 5;
 myvar.y = 6;
 console.log(myvar.x - myvar.y);
+
+//let申明的变量只在作用域里面有用，出来就没用了
 
 function foo() {
   var sum = 0;
@@ -14,14 +18,18 @@ function foo() {
 }
 foo();
 
+//const定义的是常量，就是左边的名字指向固定的地址
+
 const PI = 3.14;
 // PI = 3; 无效果！
 PI; // 3.14
 
+//只要等号两边模板一样，就可以对应赋值
+
 var [x, y, z] = ['hello', 'JavaScript', 'ES6'];
 // x, y, z分别被赋值为数组对应元素:
 console.log('x = ' + x + ', y = ' + y + ', z = ' + z);
-
+//选用对应属性名，甚至可以单独提取对象属性的值
 var person = {
   name: '小明',
   age: 20,
@@ -29,7 +37,10 @@ var person = {
   passport: 'G-12345678',
   school: 'No.4 middle school'
 };
-var {name, age, passport} = person;
+var {
+  name,
+  age,
+  passport
+} = person;
 // name, age, passport分别被赋值为对应属性:
 console.log('name = ' + name + ', age = ' + age + ', passport = ' + passport);
-
